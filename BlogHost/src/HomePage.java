8a3c5a15b54146469ba14efddc22e0ccd14b7a09
Pav <Cornhuskersfan@gmail.com>
2019-02-09 13:53:22 -0600

    test

--- BlogHost/src/HomePage.java
+++ BlogHost/src/HomePage.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HomePage
- */
 @WebServlet("/HomePage")
 public class HomePage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
