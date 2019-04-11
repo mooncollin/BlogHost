d027f8120b19e110509125542b45d599b7bfd015
Connor Pavlik <Cornhuskersfan@gmail.com>
2019-04-11 14:05:19 -0500

    Site updates. delete, line breaks and newpost

--- BlogHost/src/main/Site.java
+++ BlogHost/src/main/Site.java
@@ -5,8 +5,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Collection;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -14,15 +17,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import com.mysql.cj.util.StringUtils;
 
 //import com.mysql.cj.util.StringUtils;
 
 import dbConnection.DBConnection;
+import forms.Form;
+import forms.Input;
 import html.CompoundElement;
 import html.Element;
 import models.BlogHostPosts;
 import templates.MainTemplate;
+import user.NewPost;
 import util.Template;
+import utils.StreamUtils;
 
 @WebServlet("/Site")
 public class Site extends HttpServlet 
@@ -84,45 +94,21 @@ public class Site extends HttpServlet
     }
     protected void getSitePosts(int id) {
     	postList.clear();
-    	Connection connection = DBConnection.getDBConnection();
-    	String selectSQL = "Select * from BlogHostPosts where site_id = ?";
-        PreparedStatement preparedStatement = null;
-        try {
-        	connection = DBConnection.getDBConnection();
-	        preparedStatement = connection.prepareStatement(selectSQL);
-	        preparedStatement.setInt(1, id);
-	        ResultSet rs = preparedStatement.executeQuery();
-	       
-	        while(rs.next()) {
-	        	BlogHostPosts post = new BlogHostPosts(
-	        			rs.getInt("SITE_ID"),rs.getString("POST_TITLE"),
-	        			rs.getString("POST_TEXT"),rs.getBytes("picture"),
-	        			rs.getTimestamp("Date_Posted"),null);
-	        	
-	        	postList.add(0, post);
-	        }
-
-         } catch (Exception e) {
-            e.printStackTrace();
-         } finally {
-            try {
-               if (preparedStatement != null)
-                  preparedStatement.close();
-            } catch (Exception e2) {
-            }
-            try {
-                if (connection != null)
-                	connection.close();
-             } catch (Exception se) {
-                se.printStackTrace();
-             }
-         }
+	    postList = BlogHostPosts.getAll(BlogHostPosts.class, "site_id = ? order by id desc", id);
+	        
     }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
+		if(request.getSession().getAttribute("userSiteId") == null)
+			request.getSession().setAttribute("userSiteId",1);
 		postList.clear();
+		boolean siteSet = false;
+		//request.getSession().setAttribute("userSiteID",1);
+		if(request.getSession().getAttribute("userSiteId") != null &&  (int)request.getSession().getAttribute("userSiteId") == siteId) {
+			siteSet = true;
+		}
 		String page = request.getParameter("site");
-		if(page == null || page == "" ) {//|| !StringUtils.isStrictlyNumeric(page)) {
+		if(page == null || page == "" || !StringUtils.isStrictlyNumeric(page)) {
 			response.sendRedirect("/Error/");
 			return;
 		}
@@ -161,8 +147,7 @@ public class Site extends HttpServlet
 		header.addElement(pSite);
 		header.addElement(pCreator);
 		header.addElement(pPosts);
-		if(request.getSession().getAttribute("userSiteID") != null &&  (int)request.getSession().getAttribute("userSiteID") == siteId) {
-
+		if(siteSet) {
 		CompoundElement newPost = new CompoundElement("a", "Add New Post");
 			newPost.addClasses("btn", "btn-primary", "button");
 			newPost.setAttribute("href", "/BlogHost/NewPost");
@@ -183,7 +168,9 @@ public class Site extends HttpServlet
 			title.setData(p.getPostTitle());
 			//title.addClasses("w-75", "p-3");
 			Element text = new Element("h5");
-			text.setData(p.getPostText());
+			text.addClass("p-3 col-12");
+			text.setAttribute("id", "postText");
+			text.setData(p.getPostText().replaceAll("\n", "<br />"));
 			Element date = new Element("p");
 			date.setData("Date Posted: " + p.getDatePosted());
 			Element img = new Element("img");
@@ -192,12 +179,27 @@ public class Site extends HttpServlet
 				+Base64.getEncoder().encodeToString(p.getPicture()));
 				img.setAttribute("style","border:3px solid black;max-width:100%;");
 			}
+			Form form = new Form();
+			if(siteSet) {
+				form.setMethod("POST");
+				CompoundElement submitButton = new CompoundElement("button", "Delete Post");
+				submitButton.addClasses("btn", "btn-danger");
+				form.addElement(submitButton);
+				Element postId = new Element("input");
+				postId.setAttribute("type", "hidden");
+				postId.setAttribute("name", "postId");
+				postId.setAttribute("value", p.getID().toString());
+				form.addElement(postId);
+			}
 			item.addElement(title);
 			item.addElement(text);
-			if(p.getPicture() != null)
+			
+			if(p.getPicture() != null) {
 				item.addElement(img);
-			list.addElement(item);
+			}
 			item.addElement(date);
+			item.addElement(form);
+			list.addElement(item);
 		}
 		CompoundElement ad  = new CompoundElement("div");
 		ad.addClasses("col-2");
@@ -220,8 +222,26 @@ public class Site extends HttpServlet
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
+		Object userSiteID = request.getSession().getAttribute("userSiteId");
+		if(userSiteID == null)
+		{
+			doGet(request, response);
+			return;
+		}
 
-		doGet(request, response);
-	}
-
+		String post = request.getParameter("postId");
+		BlogHostPosts test = BlogHostPosts.getAll(BlogHostPosts.class, "id = ?", post).get(0);
+		
+		if(test.delete())
+		{
+			response.sendRedirect("/BlogHost/Site?site="+(Integer) userSiteID);
+			return;
+		}
+		else
+		{
+			System.err.println("Error in deleting Post");
+		}
+	}	
 }
+
+
